339ef2ee5d0990ecfdd3cc9fa3b0793161a5c6e6
Avinash Kumar <84294431+Avinash9608@users.noreply.github.com>
2025-01-15 15:47:23 +0530

    Update weddingBackup.js

--- routes/weddingBackup.js
+++ routes/weddingBackup.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const weddingBackupController = require("../controllers/weddingBackupController");
 // Define route for fetching all backup wedding records (Backup specific route)
-router.get("/", weddingController.getAllBackups);
+router.get("/", weddingBackupController.getAllBackups);
 
 module.exports = router;
