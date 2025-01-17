06b3a1f4abbd498e266d8d9adb26cc5d4198d9a7
Avinash Kumar <84294431+Avinash9608@users.noreply.github.com>
2025-01-17 17:27:17 +0530

    Update backupingmodel.js

--- models/backupingmodel.js
+++ models/backupingmodel.js
@@ -11,22 +11,16 @@ const backupWeddingFormSchema = new mongoose.Schema({
   brideDescription: { type: String, required: true },
   groomDescription: { type: String, required: true },
 
-  eventDetails: {
-    mainCeremony: {
+  eventDetails: [
+    {
+      type: { type: String, required: true }, // Type of party (e.g., ceremony, reception, etc.)
       title: { type: String, required: true },
-      timeStart: { type: String, required: true },
-      timeEnd: { type: String, required: true },
-      date: { type: String, required: true },
+      timeStart: { type: String, required: true }, // Start time as String
+      timeEnd: { type: String, required: true }, // End time as String
+      date: { type: String, required: true }, // Date as String
       description: { type: String, required: true },
     },
-    weddingParty: {
-      title: { type: String, required: true },
-      timeStart: { type: String, required: true },
-      timeEnd: { type: String, required: true },
-      date: { type: Date, required: true },
-      description: { type: String, required: true },
-    },
-  },
+  ],
 
   storyDetails: [
     {
